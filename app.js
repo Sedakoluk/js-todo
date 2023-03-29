971f2b2b1c0d46de12a8fdaa31b672227a6d42a8
sedakoluk <sedakoluk1@gmail.com>
2023-03-29 11:27:23 +0300

    to do
    Güncellemeler yapıldı

--- app.js
+++ app.js
@@ -1,48 +1,115 @@
-var closebtns = document.getElementsByClassName("close");
-var i;
-
-for (i = 0; i < closebtns.length; i++) {
-  closebtns[i].addEventListener("click", function(){
-    this.parentElement.style.display = 'none';
-  });
+let taskDOM = document.querySelector("#task");  // Inputa girdiğimiz değer.
+let listDOM = document.querySelector("#list");  // Listenin tamamı(ul).
+let allLiDOM = document.querySelectorAll("li"); // querySelectorAll ile tüm li'lerden oluşan array'ı tanımlıyoruz.
 
 
+// liste elemanı silme fonksiyonunu tanımladık.
+function removeElement(erase) { 
+    erase.remove();             // Liste elemanını siler
+    eraseStrorage(erase);       // Liste elemanının içeriğini localStorage'den siler
 }
 
-let x = `<span class="close">&times;</span>`// carpı işareti
-//eleman ekleme
-let inputDOM = document.querySelector('#task');
-let ulDOM = document.querySelector('#list');
-function newElement(){
-if(inputDOM.value.length>0 ){
 
-   let liDOM = document.createElement('li');
-   liDOM.innerHTML= `${inputDOM.value} ${x}` 
-   ulDOM.appendChild(liDOM)
-   liDOM.addEventListener("click", blue)
-   // çarpıya basıldığında liste elamanını kaldırmak 
-$('li').hover(function(){
-     $(this).find('span').click(function(){
-      this.parentElement.style.display = 'none';
-     })
+// Liste elemanını işaretleme fonksiyonunu tanımladık.
+function markElement(){
+    this.classList.toggle("checked");
+}
+
+
+// listelerdeki kapama tuşu değişkenini liste elemanı silme eventi ile tanımladık.
+let closeButton = `<button 
+onclick="removeElement(parentNode)" 
+style="padding: 13px;" type="button" 
+class="close" 
+data-dismiss="toast"
+aria-label="Close">
+<span aria-hidden="true">&times;</span>
+</button>`
+
+
+// Her bir liste elemanı için hem kapama tuşu(liste elemanı silme eventi ile) hem de liste elemanı işaretleme eventi ile tanımlıyoruz.
+allLiDOM.forEach(e => {
+    e.addEventListener("click", markElement);
+    e.innerHTML += `${closeButton}`;
 })
-//yeni elemanlara check classı eklemek 
-  function blue(){
-    liDOM.className == "checked" ? liDOM.classList.remove("checked"): liDOM.classList.add("checked")
 
+
+// Liste elemanı ekleme fonksiyonunu tanımladık.
+function newElement() {
+
+    if (!taskDOM.value || !taskDOM.value.trim()) {
+        console.log("Listeye boş ekleme yapamazsınız!");
+        $(".error").toast('show') // toast bildirimi 1
+    }
+
+    else {
+        // Liste için yeni bir li elemanı oluşturduk.
+        let liDOM = document.createElement("li"); 
+        
+        // Bu li elemanını liste(ul) içerisine yerleştirdik.
+        listDOM.append(liDOM); 
+                                
+        console.log(`Listeye eklendi. Girilen değer: ${taskDOM.value}`)
+        
+        // Toast bildirimi - başarılı
+        $(".success").toast('show') 
+
+        liDOM.innerHTML = `${taskDOM.value}${closeButton}`;
+        // li elemanının içeriğine inputa girilen değeri ve kapama tuşu tanımladık.
+
+        // li elemanı için işaretleme fonksiyonunu eventi tanımladık.
+        liDOM.addEventListener("click", markElement);
+
+        // localStorage.setItem("inputValue", `${taskDOM.value}`);
+        setStrorage()
+    }  
+
+    taskDOM.value = ""; // input'u sıfırladık.
 }
 
-   $(".success").toast('show')
-}else{
-   $(".error").toast('show')
-}}
 
-// eski elemanlara check classı eklemek 
+// Buradan sonra tanımladığımız fonksiyonlar tamamen localStorage ile ilgilidir.
 
-let liAll = document.querySelectorAll('li');
-for(let i =0; i<liAll.length;i++) {
-   console.log(i)
-   liAll[i].addEventListener("click", function(){
-      liAll[i].className == "checked" ? liAll[i].classList.remove("checked"): liAll[i].classList.add("checked")
-   })
-}   
+
+// Liste elemanı içeriğini localStorage'ye eklediğimiz fonksiyon:
+function setStrorage(){
+    let toDoList = JSON.parse(localStorage.getItem("toDoList"));   // toDoList ls'sini array'a çevirip olarak çağırdık.
+    toDoList.push(`${taskDOM.value}`);                             // input'a girdiğimiz yazıyı toDoList array'ine ekledik.
+    localStorage.setItem("toDoList", JSON.stringify(toDoList));    // toDoList'i tekrar string'e çevirip ls'ye yolladık.
+}
+
+
+// Liste elemanının içeriğini localStorage'den sildiğimiz fonksiyon:
+function eraseStrorage(erase){
+    let toDoList = JSON.parse(localStorage.getItem("toDoList"));    // toDoList ls'sini array'a çevirip olarak çağırdık.
+    if (toDoList.includes(erase.firstChild.textContent) == true) {  // toDoList array'i listeye yazdığımız metini içeriyorsa
+        let indexbul = toDoList.findIndex(e =>                      // Bu metinin(array'in elemanı) index nosunu buluyoruz.
+            e == erase.firstChild.textContent
+            );
+        toDoList.splice(indexbul, 1);                               // index nosundan kendisini bulup array'den siliyoruz.
+        localStorage.setItem("toDoList", JSON.stringify(toDoList)); // toDoList'i tekrar string'e çevirip ls'ye yolladık.
+    } 
+}
+
+// Eğer daha önce oluşturulmuş bir localStorage dosyası yok ise oluşturan fonksiyon:
+function localSelf() {
+    let toDoList = JSON.parse(localStorage.getItem("toDoList"));    // toDoList ls'sini array'a çevirip olarak çağırdık.
+    if (!toDoList) {toDoList = []};                                 // Hali hazırda toDoList array'i yoksa biz oluşturduk.
+    localStorage.setItem("toDoList", JSON.stringify(toDoList));     // toDoList'i tekrar string'e çevirip ls'ye yolladık.
+}
+
+// Sayfayı her açtığımızda localStorage'de bulunan her elemanı listeye ekleyen fonksiyon:
+function localDOM(){
+    let toDoList = JSON.parse(localStorage.getItem("toDoList"));    // toDoList ls'sini array'a çevirip olarak çağırdık.
+    toDoList.forEach((e, index) => {                                // toDoList'de kayıtlı her eleman ve index nosu için;
+        let liDOM = document.createElement("li");                   // liste elemanı oluşturduk.
+        listDOM.append(liDOM);                                      // bu liste elemanlarını listeye(ul) ekledik.
+        liDOM.innerHTML = toDoList[index]                           // liste elemanlarının içeriğine toDoList'teki arrayler,
+        liDOM.innerHTML += `${closeButton}`                         // ve kapama tuşu ekledik.
+        liDOM.addEventListener("click", markElement);               // işaretleme eventi ekledik.
+    })
+}
+
+localSelf() // Yok ise localStorage dosyası oluşturuldu.
+
+localDOM()  // localStorage'de daha önce kaydettiğimiz liste elemanları DOM arayüzüne eklendi.
